ecead800e616cbf959ab23e1718807d0af3afec1
daixu44 <1504625040@qq.com>
2023-11-23 20:01:47 +0800

    定义了item将，写到了pipeline步骤

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -10,3 +10,15 @@ class ArticlespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class CnblogsspiderItem(scrapy.Item):
+    news_title = scrapy.Field()
+    create_date = scrapy.Field()
+    news_body = scrapy.Field()
+    news_tags = scrapy.Field()
+    praise_num = scrapy.Field()
+    view_num = scrapy.Field()
+    dislike_num = scrapy.Field()
+    comment_num = scrapy.Field()
+
